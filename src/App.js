3a82419dbeffd3dd9d3f9c4d9567f56cef096eaa
wfinazzi <wellingtonfinazzi@gmail.com>
2016-11-12 15:21:59 -0200

    eh nois parse

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ import Menu from './components/Menu.jsx';
 import Botao from './components/BotaoAbrir';
 import Barra from './components/Barra.jsx';
 import Footer from './components/Footer.jsx';
+import Parse from 'parse';
+import ParseReact from 'parse-react';
 
 
 // função auxiliar que abre e fecha o menu
 
 
-
+Parse.initialize('OBH1UBDdv4al3h2Br8qJOFTfyCyMIw816hnooZf3','s5X6vSQRWTMbRlPPuLceKWHOxpKTpXpaimXQZM2v');
+Parse.serverURL='https://parseapi.back4app.com/';
+var Politico = Parse.Object.extend('Politico');
 
 class App extends Component {
 
+    
+    
+    
     render() {
         return (
             <div className="wrapper">
